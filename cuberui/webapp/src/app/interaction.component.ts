1810904d66ee99d45bb4e24ec03a528121cda7fd
ramsub123 <ramsub123@gmail.com>
2017-05-15 16:16:27 -0700

    This fixes all the issues, next will be some cool UI changes.

--- cuberui/webapp/src/app/interaction.component.ts
+++ cuberui/webapp/src/app/interaction.component.ts
@@ -1,45 +1,42 @@
 /**
  * Created by ramsub on 5/2/17.
  */
-import {ChangeDetectionStrategy, Component} from "@angular/core";
+import {ChangeDetectionStrategy, Component, Injectable, Input} from "@angular/core";
 import {RealTime} from "./shared/sdk/services";
 import {FireLoopRef} from "./shared/sdk/models/FireLoopRef";
 
 import {Interaction} from "./shared/sdk/models";
-import {Observable} from "rxjs";
+import {Observable} from "rxjs/Observable";
+import {Subject} from "rxjs/Subject";
+import {AsyncSubject} from "rxjs/AsyncSubject";
 
 
 @Component({
   selector: 'all-interactions',
   templateUrl: './interaction.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.Default
 })
 
+ @Injectable()
 export class InteractionComponent {
   public InterRef: FireLoopRef<Interaction>;
-  private interarray: Array<Interaction> = new Array<Interaction>();
-  public inters: Observable<Array<Interaction>>;
+  public interarray: Array<Interaction> = new Array<Interaction>();
+  public inters: Observable<Interaction[]>;
 
   constructor(private rt: RealTime) {
-
-    this.inters = Observable.create(this.interarray);
+    this.interarray.push(new Interaction( {"source":"AI","text":"Hello there...","image":""} ));  
+	this.inters = Observable.of( this.interarray );	
 
     this.rt.onReady().subscribe(() => {
       this.InterRef = this.rt.FireLoop.ref<Interaction>(Interaction);
-
-      // this.inters = this.InterRef.on('changes',{});
-
-      this.rt.IO.on('HumanSaid').subscribe((message: Observable<any>) => {
-        console.log('entering inside InterAction Component HumanSaid', message);
-
-        // this.inters.merge(message);
-
-
-        var subscription = this.inters.forEach(x => console.log('outputting inter : ', x));
-
-
-        console.log('leaving inside InterAction Component HumanSaid', message);
-      });
     });
+
+	  this.rt.IO.on('HumanSaid').subscribe((message: string) => {
+	    console.log('entering inside InterAction Component HumanSaid', message);
+	    var linter : Interaction = JSON.parse(message);
+		this.interarray.push(linter);
+	    console.log('leaving inside InterAction Component HumanSaid', message);
+	  });
+
   }
 }
