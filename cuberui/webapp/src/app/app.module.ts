3624da88341f2c6752e4137c71f69e9159a5f7aa
ramsub123 <ramsub123@gmail.com>
2017-05-16 14:47:55 -0700

    After multiple attempts, this is the version that also auto scrolls.

--- cuberui/webapp/src/app/app.module.ts
+++ cuberui/webapp/src/app/app.module.ts
@@ -4,14 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule} from '@angular/http';
 import { SDKBrowserModule } from './shared/sdk/index';
 import { AppComponent } from './app.component';
-import {HumanInteractionComponent} from "./human-interaction.component";
-import {InteractionComponent} from "./interaction.component";
+import {HumanInteractionComponent} from './human-interaction.component';
+import {InteractionComponent} from './interaction.component';
+import {Angular2AutoScroll} from 'angular2-auto-scroll/lib/angular2-auto-scroll.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     HumanInteractionComponent,
-    InteractionComponent
+    InteractionComponent,
+    Angular2AutoScroll
   ],
   imports: [
     BrowserModule,
