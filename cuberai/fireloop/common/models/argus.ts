3dff93f2146be3dbc6cdeaf5e00f7d6471dfc7fd
ramsub123 <ramsub123@gmail.com>
2017-06-01 09:36:54 -0700

    Committing to carry my work with me.

---
+++ cuberai/fireloop/common/models/argus.ts
@@ -0,0 +1,76 @@
+import { Model } from '@mean-expert/model';
+/**
+ * @module argus
+ * @description
+ * Write a useful argus Model description.
+ * Register hooks and remote methods within the
+ * Model Decorator
+ **/
+@Model({
+  hooks: {
+    beforeSave: { name: 'before save', type: 'operation' }
+  },
+  remotes: {
+    myRemote: {
+      returns : { arg: 'result', type: 'array' },
+      http    : { path: '/my-remote', verb: 'get' }
+    },
+    create: {
+      accepts: [{arg: 'channels', type: 'string'},{arg:'clientID', type:'string'}],
+      return: {arg: 'channelID', type: 'string'},
+      http: {path: '/create', verb: 'post'}
+    }
+  }
+})
+
+class argus {
+  // LoopBack model instance is injected in constructor
+  constructor(public model: any) {}
+
+  // Example Operation Hook
+  beforeSave(ctx: any, next: Function): void {
+    console.log('argus: Before Save');
+    next();
+  }
+  // Example Remote Method
+  myRemote(next: Function): void {
+    this.model.find(next);
+  }
+        
+  create(channels: string[], clientID : string): string {
+    console.log('entering argus.create', channels);
+
+    /*
+     * channels - is a array of string which will indicate 
+     *   - human    = interaction is with human
+     *   - machine  = originating from machine  
+     *   - text     = text entered by human or maybe a website
+     *   - sound    = the stream is going to audio
+     *   - image    = the stream is an image
+     *   - video    = the stream is a video stream
+     */
+    
+    // I wanted to use Set, but looks like Set datatype is not available in Typescript
+      
+      
+    // The alternative would be to use a JSON
+      
+    /*
+      {
+         "text"  : ["human","machine","any"],
+         "audio" : ["speech","music","any"],
+         "image" : ["photo","drawing","art", "id","any"],
+         "video" : ["people", "monitoring", "any"],
+         "event" : ["transaction", "chat", "any"]
+      }
+     */
+    
+    console.log('exiting argus.create');
+
+    return '';
+  }
+        
+        
+}
+
+module.exports = argus;
---
+++ cuberai/webapp/src/app/shared/sdk/models/index.ts
@@ -0,0 +1,5 @@
+/* tslint:disable */
+export * from './User';
+export * from './Argus';
+export * from './BaseModels';
+export * from './FireLoopRef';
